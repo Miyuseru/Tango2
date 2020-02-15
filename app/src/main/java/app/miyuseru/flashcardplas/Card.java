40744c4cb5a4979b877d038ccadbe7dc62f91f60
miyuu kuwana <miyuu13k@gmail.com>
2020-02-15 22:40:01 +0900

    TODO2

---
+++ app/src/main/java/app/miyuseru/flashcardplas/Card.java
@@ -0,0 +1,16 @@
+package app.miyuseru.flashcardplas;
+
+public class Card {
+
+    class  Card(
+    var ImageId: Int ,
+    var title:String,
+    var content:String,
+    var meaning:String,
+    var likenum: Int)
+
+
+
+}
+
+
